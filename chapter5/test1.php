e051f1b62c703a078ccbe8396eda789e20331854
jeonwonmin <jjwonmin@naver.com>
2015-01-12 04:39:02 +0900

    first commit

---
+++ chapter5/test1.php
@@ -0,0 +1,19 @@
+<?php
+    //$sum = 1981 + 35;
+    //echo $sum;
+    
+    $value = 99999999999997;
+    echo $value."<br>";
+    $value += 1;
+    echo $value."<br>";
+    $value += 1;
+    echo $value."<br>";
+    $value += 1;
+    echo $value."<br>";
+    $value += 1;
+    echo $value."<br>";
+    $value += 1;
+    echo $value."<br>";
+    $value += 1;
+    echo $value."<br>";
+?>    
